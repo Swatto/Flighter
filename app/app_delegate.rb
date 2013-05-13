fa6b6f45809f7cc3b69b79d944f7ee4677557adc
Gaël Gillard <gillardgael@gmail.com>
2013-05-13 13:39:05 +0200

    new screenshot and remove stats from app_delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,7 +3,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
     @director = Joybox::Configuration.setup do
-      director display_stats: true
+      # director display_stats: true
     end
 
     @navigation_controller = UINavigationController.alloc.initWithRootViewController(@director)
