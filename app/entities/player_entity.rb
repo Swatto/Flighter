0e7ba64a7fd703af8fa558dec4fb219db4860971
CurveBeryl <public.juan.karam@me.com>
2013-07-28 16:02:20 -0500

    Updated to Joybox 1.1.0

--- app/entities/player_entity.rb
+++ app/entities/player_entity.rb
@@ -3,9 +3,10 @@ class PlayerEntity < CCNode
 
   def initWithWorld(world)
     self.up = false
-    self.body = world.new_body(position: [Screen.half_width,Screen.half_height], type: KDynamicBodyType)do
+    self.body = world.new_body(position: Screen.center, type: Body::Dynamic)do
       polygon_fixture(box:[30,30],friction: 0.0,density: 100.0)
     end
+    self.body.fixed_rotation = true
 
     self.sprite = PhysicsSprite.new(
       file_name: 'sprites/player.png',
@@ -16,9 +17,10 @@ class PlayerEntity < CCNode
 
   def update(dt)
     if up
-      self.body.apply_force force: ([0,1000])
+      self.body.apply_force force: ([0, 1000])
     end
-    if self.body.position.y < -3 || self.body.position.y>12 || self.body.position.x < -3
+
+    if self.body.position.y < 0 || self.body.position.y > Screen.height
       director = Joybox.director
       director.replace_scene MenuLayer.scene
     end
